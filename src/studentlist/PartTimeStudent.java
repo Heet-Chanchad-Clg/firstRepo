7e3661f05a8f81bea0ee86c8fbe55c33ccf7749f
heet <chanchah@sheridancollege.ca>
2024-09-17 09:51:30 -0400

    Made a part time student file and getters ans setters.

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author heetc
+ */
+public class PartTimeStudent {
+    
+    private int studentId;
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+    
+    
+    
+}
